62415741776e488d02fb2924ab1d7054a610cd23
xistne <du20063@gmail.com>
2024-07-22 21:46:45 +0900

    Chapter 6. 백엔드 개발에 필요한 HTML

--- hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
+++ hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
@@ -1,6 +1,7 @@
 package kr.co.hanbit;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,11 @@ public class SimpleRestController {
     @RequestMapping("/bye")
     public String bye() {
         return "Bye";
+    }
+
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title,
+                                @RequestParam("content") String content){
+        return String.format("title=%s / content=%s", title, content);
     }
 }
