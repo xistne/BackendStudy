0def85aba446d5fad99d74f3b7199d6a86ac441d
xistne <du20063@gmail.com>
2024-10-16 22:39:13 +0900

    리팩토링 (ModelMapper -> 직접 구현)

--- product.management/src/main/java/kr/co/hanbit/product/management/Application.java
+++ product.management/src/main/java/kr/co/hanbit/product/management/Application.java
@@ -1,7 +1,5 @@
 package kr.co.hanbit.product.management;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,15 +14,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration()
-					.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
-					.setFieldMatchingEnabled(true);
-		return modelMapper;
 	}
 
 	@Bean
--- product.management/src/main/java/kr/co/hanbit/product/management/presentation/ProductDto.java
+++ product.management/src/main/java/kr/co/hanbit/product/management/presentation/ProductDto.java
@@ -1,6 +1,7 @@
 package kr.co.hanbit.product.management.presentation;
 
 import jakarta.validation.constraints.NotNull;
+import kr.co.hanbit.product.management.domain.Product;
 
 public class ProductDto {
     private Long id;
@@ -29,5 +30,40 @@ public class ProductDto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public ProductDto(String name, Integer price, Integer amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public ProductDto(Long id, String name, Integer price, Integer amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public static Product toEntity(ProductDto productDto) {
+        Product product = new Product(
+                productDto.getId(),
+                productDto.getName(),
+                productDto.getPrice(),
+                productDto.getAmount()
+        );
+
+        return product;
+    }
+
+    public static ProductDto toDto(Product product) {
+        ProductDto productDto = new ProductDto(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getAmount()
+        );
+        productDto.setId(product.getId());
+        return productDto;
     }
 }
